5bbbe60f96e1bc976cd136254104cdc80297b3fc
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-09-08 17:51:09 -0400

    Create Station

---
+++ FactoryLayoutProblem/src/main/java/factoryLayout/Station.java
@@ -0,0 +1,25 @@
+package factoryLayout;
+
+public class Station {
+  private final int r;
+  private final int g;
+  private final int b;
+
+  public Station(int r, int g, int b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+
+  public int getR() {
+    return r;
+  }
+
+  public int getG() {
+    return g;
+  }
+
+  public int getB() {
+    return b;
+  }
+}
